4b20c25fc52eec7225acf950d02142dc2d643324
Ricardo Torres <73494209+Ricardotss@users.noreply.github.com>
2023-03-11 22:25:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    num_avaliacoes = 3
-    return render_template("index.html", num_avaliacoes=num_avaliacoes)
+    if request.method == 'POST':
+        # Extrai as informações do formulário
+        num_avaliacoes = int(request.form['num_avaliacoes'])
+        notas = []
+        pesos = []
+        for i in range(num_avaliacoes):
+            nota = float(request.form['nota{}'.format(i)])
+            peso = float(request.form['peso{}'.format(i)])
+            notas.append(nota)
+            pesos.append(peso)
+        media_aprovacao = float(request.form['media_aprovacao'])
 
+        # Calcula a nota final e determina a mensagem
+        nota_final = sum([notas[i] * pesos[i] for i in range(num_avaliacoes)]) / (sum(pesos) * 1.0)
+        gpa = nota_final / 20 - 1
+        mensagem = ''
+        if nota_final >= media_aprovacao:
+            mensagem = 'Parabéns, você foi aprovado!'
+        else:
+            nota_necessaria = (media_aprovacao - sum([notas[i] * pesos[i] for i in range(num_avaliacoes)])) / (1 - sum(pesos))
+            mensagem = f'Você precisa de uma nota de {nota_necessaria:.2f} na próxima avaliação para ser aprovado.'
 
-@app.route('/calcular', methods=['POST'])
-def calcular():
-    # Obter as notas das avaliações
-    num_avaliacoes = int(request.form['num_avaliacoes'])
-
-    notas = []
-
-    for i in range(num_avaliacoes):
-        nota = float(request.form['nota_{}'.format(i)])
-        notas.append(nota)
-
-    # Calcular a média ponderada de notas
-    pesos = [1/num_avaliacoes] * num_avaliacoes
-    nota_final = sum([notas[i]*pesos[i] for i in range(num_avaliacoes)])
-
-    # Obter a meta de GPA e calcular a nota necessária
-    media_aprovacao = float(request.form['media_aprovacao'])
-    meta_gpa = media_aprovacao
-    if nota_final < media_aprovacao:
-        nota_necessaria = (meta_gpa - nota_final*(num_avaliacoes/10)) / (1-(num_avaliacoes/10))
-        mensagem = "Para alcançar sua nota para aprovação, você precisa de uma nota de pelo menos {:.2f} na próxima avaliação.".format(nota_necessaria)
+        # Renderiza a página de resultado com as informações calculadas
+        return render_template("resultado.html", nota_final=nota_final, mensagem=mensagem)
     else:
-        mensagem = "Parabéns! Você já está aprovado."
-    
-    return render_template('resultado.html', nota_final=nota_final, mensagem=mensagem)
+        num_avaliacoes = 3
+        return render_template("index.html", num_avaliacoes=num_avaliacoes)
 
 if __name__ == '__main__':
     app.run(debug=True)
